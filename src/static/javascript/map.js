b8fa47f044f379e8d4f99594ab78d09629493d8b
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-21 16:58:00 +0900

    :construction: show marker on map view

--- src/static/javascript/map.js
+++ src/static/javascript/map.js
@@ -1,3 +1,14 @@
-$(function() {
-  
-});
+//leaflet OSM map
+function init() {
+
+  var map = L.map('map_elemnt')
+    .setView([35.681382, 139.766084], 15);
+
+  var googlemap = new L.Google('HYBRID');
+  map.addLayer(googlemap);
+
+  L.marker([35.681382, 139.766084])
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup()
+    .addTo(map);
+}
