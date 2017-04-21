12e97391c997f6483569b275edf21f5db6571557
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-22 01:38:55 +0900

    :construction: set marker on click position

--- src/static/javascript/map.js
+++ src/static/javascript/map.js
@@ -1,15 +1,22 @@
 //leaflet OSM map
-function init() {
+var map = L.map('map_elemnt')
+  .setView([35.681382, 139.766084], 15);
 
-  var map = L.map('map_elemnt')
-    .setView([35.681382, 139.766084], 15);
+L.gridLayer.googleMutant({
+  type: 'hybrid'
+}).addTo(map);
 
-  L.gridLayer.googleMutant({
-    type: 'hybrid'
-  }).addTo(map);
 
-  L.marker([35.681382, 139.766084])
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+var marker = L.marker([35.681382, 139.766084])
+  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+  .openPopup()
+  .addTo(map);
+
+var popup = L.popup();
+function onMapClick(e) {
+  marker.setLatLng(e.latlng)
+    .bindPopup("You clicked the map at " + e.latlng.toString())
     .openPopup()
-    .addTo(map);
 }
+
+map.on('click',onMapClick);
