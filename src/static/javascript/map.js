44bb81bcb517d963e1c1d8bd2ba612edde689433
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-21 21:35:27 +0900

    :construction: :rocket: change using plugin Google.js-> GoogleMutant.js

--- src/static/javascript/map.js
+++ src/static/javascript/map.js
@@ -4,8 +4,9 @@ function init() {
   var map = L.map('map_elemnt')
     .setView([35.681382, 139.766084], 15);
 
-  var googlemap = new L.Google('HYBRID');
-  map.addLayer(googlemap);
+  L.gridLayer.googleMutant({
+    type: 'hybrid'
+  }).addTo(map);
 
   L.marker([35.681382, 139.766084])
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
