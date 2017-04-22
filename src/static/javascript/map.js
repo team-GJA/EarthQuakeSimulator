77d6d96268dd459a7d2356b4f960cb92754f5627
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-22 18:54:39 +0900

    :construction: :rocket: set observation, epicenter and params marker on click

--- src/static/javascript/map.js
+++ src/static/javascript/map.js
@@ -1,4 +1,4 @@
-//leaflet OSM map
+//init and set map
 var map = L.map('map_elemnt')
   .setView([35.681382, 139.766084], 15);
 
@@ -7,16 +7,57 @@ L.gridLayer.googleMutant({
 }).addTo(map);
 
 
-var marker = L.marker([35.681382, 139.766084])
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup()
-  .addTo(map);
+var obspopup = "<p class=\"popup\">set the observation marker here?</p>"+
+  "<button id=\"obscancel\" onclick=\"deleteObsMarker()\">CANCEL</button>"+
+  "<button id=\"obsok\" onclick=\"setEpiMarker()\">OK</button>";
 
-var popup = L.popup();
-function onMapClick(e) {
-  marker.setLatLng(e.latlng)
-    .bindPopup("You clicked the map at " + e.latlng.toString())
-    .openPopup()
+var epipopup = "<p class=\"popup\">set the epicenter marker here?</p>"+
+  "<button id=\"epicancel\" onclick=\"deleteEpiMarker()\">CANCEL</button>"+
+  "<button id=\"obsok\" onclick=\"setParam()\">OK</button>";
+
+var parampopup = "<p class=\"popup\">set Magnitude and Depth</p>"+
+  "<button id=\"paramcancel\" onclick=\"resetEpiMarker()\">CANCEL</button>"+
+  "<button id=\"paramok\" onclick=\"setParam()\">OK</button>";
+
+
+//define marker object
+var obsMarker = L.marker().bindPopup(obspopup);
+var epiMarker = L.marker().bindPopup(epipopup);
+var isSetEpiMarker = false;
+
+
+var onMapClick = function(e) {
+  if(!isSetEpiMarker){
+    obsMarker.setLatLng(e.latlng)
+      .addTo(map).openPopup();
+  }else{
+    epiMarker.setLatLng(e.latlng)
+      .addTo(map).openPopup();
+  }
+}
+
+
+function deleteObsMarker() {
+  obsMarker.remove();
+  isSetEpiMarker = false;
+}
+
+function setEpiMarker() {
+  isSetEpiMarker = true;
+}
+
+function deleteEpiMarker() {
+  epiMarker.remove();
+  isSetEpiMarker = false;
+}
+
+function resetEpiMarker(){
+  epiMarker.remove();
+  epiMarker.bindPopup(epipopup);
+}
+
+function setParam() {
+  epiMarker.bindPopup(parampopup).openPopup();
 }
 
 map.on('click',onMapClick);
