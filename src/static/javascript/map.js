77858a28e09d04368acb06d4dd89245e268233c6
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-25 14:25:31 +0900

    :construction: :heavy_plus_sign: Add bootstrap-Material, fix slider in popup

--- src/static/javascript/map.js
+++ src/static/javascript/map.js
@@ -6,17 +6,23 @@ L.gridLayer.googleMutant({
   type: 'hybrid'
 }).addTo(map);
 
+
+//define popup contents
 var obspopup = "<p class=\"popup\">set the observation marker here?</p>"+
-  "<button id=\"obscancel\" onclick=\"deleteObsMarker()\">CANCEL</button>"+
-  "<button id=\"obsok\" onclick=\"setEpiMarker()\">OK</button>";
+  "<button class=\"btn btn-default\" id=\"obscancel\" onclick=\"deleteObsMarker()\">CANCEL</button>"+
+  "<button class=\"btn btn-default\" id=\"obsok\" onclick=\"setEpiMarker()\">OK</button>";
 
 var epipopup = "<p class=\"popup\">set the epicenter marker here?</p>"+
-  "<button id=\"epicancel\" onclick=\"deleteEpiMarker()\">CANCEL</button>"+
-  "<button id=\"obsok\" onclick=\"setParam()\">OK</button>";
+  "<button class=\"btn btn-default\" id=\"epicancel\" onclick=\"deleteEpiMarker()\">CANCEL</button>"+
+  "<button class=\"btn btn-default\" id=\"obsok\" onclick=\"setEQParam()\">OK</button>";
 
 var parampopup = "<p class=\"popup\">set Magnitude and Depth</p>"+
-  "<button id=\"paramcancel\" onclick=\"resetEpiMarker()\">CANCEL</button>"+
-  "<button id=\"paramok\" onclick=\"setParam()\">OK</button>";
+  "<p>Magnitude(Mj)</p>"+
+  "<div id=\"mag-slider\" class=\"slider\"></div><input id=\"mag-input\"></input><br>"+
+  "<p>Depth(km)</p>"+
+  "<div id=\"dep-slider\" class=\"slider shor slider-material-orange\"></div><input id=\"dep-input\"></input><br>"+
+  "<button class=\"btn btn-default\" id=\"paramcancel\" onclick=\"resetEpiMarker()\">CANCEL</button>"+
+  "<button class=\"btn btn-default\" id=\"paramok\" onclick=\"\">OK</button>";
 
 
 //define marker object
@@ -24,16 +30,22 @@ var obsMarker = L.marker().bindPopup(obspopup);
 var epiMarker = L.marker().bindPopup(epipopup);
 var isSetEpiMarker = false;
 
+
 var onMapClick = function(e) {
   if(!isSetEpiMarker){
+    obsMarker.remove();
     obsMarker.setLatLng(e.latlng)
+      .bindPopup(obspopup)
       .addTo(map).openPopup();
   }else{
+    epiMarker.remove();
     epiMarker.setLatLng(e.latlng)
+      .bindPopup(epipopup)
       .addTo(map).openPopup();
   }
 }
 
+map.on('click',onMapClick);
 
 function deleteObsMarker() {
   obsMarker.remove();
@@ -55,8 +67,35 @@ function resetEpiMarker(){
   epiMarker.bindPopup(epipopup);
 }
 
-function setParam() {
+function setEQParam() {
+  var magSlider, depSlider, magInput, depInput;
   epiMarker.bindPopup(parampopup).openPopup();
+  setSlider(magSlider, magInput, 'mag', [5,1,9]);
+  setSlider(depSlider, depInput, 'dep', [100,10,900]);
+
 }
 
-map.on('click',onMapClick);
+function setSlider(slider, input, id, range) {
+  slider = document.getElementById(id+'-slider');
+  input = document.getElementById(id+'-input');
+
+  noUiSlider.create(slider, {
+    start: range[0],
+    range: {
+      'min': range[1],
+      'max': range[2]
+    }
+  });
+
+  slider.noUiSlider.on('update', function( values, handle ) {
+    input.value = values[handle];
+  });
+
+  input.addEventListener('change', function(){
+      slider.noUiSlider.set(this.value);
+  });
+}
+
+$(function(){
+  $.material.init();
+});
