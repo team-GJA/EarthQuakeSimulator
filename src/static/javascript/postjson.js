6cf5aeb77f8dcea295886b551c81efa7747808eb
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-29 15:20:00 +0900

    :heavy_plus_sign: Add manual and auto mode post function

--- src/static/javascript/postjson.js
+++ src/static/javascript/postjson.js
@@ -1,4 +1,4 @@
-function postJson() {
+function postJsonManualmode() {
   var button = $(this);
   button.attr("disabled", true);
 
@@ -9,7 +9,7 @@ function postJson() {
   console.log(data);
   $.ajax({
     type:"post",
-    url:"/json",
+    url:"/sim/manual",
     data:JSON.stringify(data),
     contentType: 'application/json',
     dataType: "json",
@@ -28,3 +28,33 @@ function postJson() {
     }
   });
 };
+
+function postJsonAutomode() {
+  var button = $(this);
+  button.attr("disabled", true);
+
+  var data = {
+    observation: {lat: obsMarker.getLatLng().lat, long: obsMarker.getLatLng().lng}
+  };
+  console.log(data);
+  $.ajax({
+    type:"post",
+    url:"/sim/auto",
+    data:JSON.stringify(data),
+    contentType: 'application/json',
+    dataType: "json",
+    success: function(json_data) {
+      if (!json_data[0]) {
+        alert("Transaction error. " + json_data[1]);
+        return;
+      }
+      location.reload();
+    },
+    error: function() {
+      alert("Server Error. Pleasy try again later.");
+    },
+    complete: function() {
+      button.attr("disabled", false);
+    }
+  });
+}
