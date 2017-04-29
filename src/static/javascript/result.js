16eb13c255e077a60de9794e0253ca83d6d57acb
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-04-29 19:49:45 +0900

    :rocket: add seismic intencity and tips in popup, add restart button

--- src/static/javascript/result.js
+++ src/static/javascript/result.js
@@ -1,3 +1,4 @@
+var circleArray = new Array();
 function showResult(json_data){
   console.log(json_data);
 
@@ -15,14 +16,31 @@ function showResult(json_data){
 
   for(var i=0;i<9;i++){
     if(json_data.Scaleranges[i] != 0){
-      L.circle(epicenterPos, {
+      var circle =  L.circle(epicenterPos, {
         opacity: 0,
         fillColor: seismicIntensityColor[i],
         fillOpacity: 0.3,
         radius: json_data.Scaleranges[i]*1000,
         fillRule:"inherit"
       }).addTo(map);
+      circleArray.push(circle);
     }
   }
-  obsMarker.bindPopup("震度"+json_data.Scale).openPopup();
+
+  obsMarker.bindPopup(
+    "<p class=\"popupSeismic\">震度"+json_data.Scale+"の揺れを観測しました！</p>"+
+    "<p class=\"popupTips\"> Tips:<br>"+json_data.Tips+"</p>"+
+    "<button class=\"btn btn-default btn-warning\" id=\"paramrun\" onclick=\"restart()\">Restart</button>"
+    ,{maxWidth:600}
+  ).openPopup();
+}
+
+function restart(){
+  for(i = 0, l = circleArray.length; i < l; i++){
+    circleArray[i].remove();
+  }
+  isSetEpiMarker = false;
+  obsMarker.remove();
+  epiMarker.remove();
+  map.on('click',onMapClick);
 }
