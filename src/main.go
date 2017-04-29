3852abb4e8c278820ec6d4c50598e1cf3041b1b9
Funasaka Takumi <takumi.funasaka@gmail.com>
2017-04-29 13:35:45 +0900

    improve routing 2

--- src/main.go
+++ src/main.go
@@ -11,22 +11,26 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Position struct {
+	Lat float32 `json:"lat"`
+	Long float32 `json:"long"`
+}
+
+type Observation struct {
+	Pos Position `json:"pos"`
+}
+
+type Epicenter struct {
+	Pos Position `json:"pos"`
+	Mag float32 `json:"mag"`
+}
 
 type Input struct {
 	Observation Observation `json:"observation"`
 	Epicenter   Epicenter   `json:"epicenter"`
-}
-
-type Observation struct {
-	Lat  float32 `json:"lat"`
-	Long float32 `json:"long"`
-}
-
-type Epicenter struct {
-	Lat       float32 `json:"lat"`
-	Long      float32 `json:"long"`
-	Magnitude float32 `json:"magnitude"`
 }
 
 type Output struct {
@@ -34,10 +38,30 @@ type Output struct {
 	Result string
 	Scale float32
 	Scaleranges []int
+	Observation Position
+	Epicenter Position
 }
 
-func jsonHandler(rw http.ResponseWriter, req *http.Request) {
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/sim/{mode}", JsonHandler)
+
+	// 静的ファイルの提供
+	// $PROROOT/template/map.html が http://localhost:8080/template/map.html でアクセスできる
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func JsonHandler(rw http.ResponseWriter, req *http.Request) {
+	fmt.Print("jsonHandler entered.")
 	output := Output{ Status: 0, Result: "default", Scale: 0, Scaleranges: []int{0,0,0,0,0,0,0,0}}
+
+	vars := mux.Vars(req)
+	output.Result = vars["mode"] + "モードでアクセスを受けました"
+
 
 	defer func() {
 		outjson, e := json.Marshal(output)
@@ -73,45 +97,36 @@ func jsonHandler(rw http.ResponseWriter, req *http.Request) {
 	}
 
 	//parse json
-	var jsonBody Input
-	err = json.Unmarshal(body[:length], &jsonBody)
+	var input Input
+	err = json.Unmarshal(body[:length], &input)
 	if err != nil {
 		output.Status = 1
 		output.Result = "Parse Error!"
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	//震度　= -1.872377 + 0.947802 * マグニチュード　- 0.004825 * 震央距離
-	//distance = np.sqrt(((minpoint[0] - longitude) / 0.0111) ** 2 + ((minpoint[1] - latitude) / 0.0091) ** 2)
 
-	//magnitude = 8.0
-
-	//earthquake = np.random.normal(-1,1) + 0.947802 * magnitude - 0.004825 * distance
 	randvalue := rand.NormFloat64()
-	output.Scale = float32(randvalue + float64(0.947802 * jsonBody.Epicenter.Magnitude) - 0.004825 * math.Sqrt(float64(((jsonBody.Epicenter.Long - jsonBody.Observation.Long) / 0.0111) * ((jsonBody.Epicenter.Long - jsonBody.Observation.Long) / 0.0111) + ((jsonBody.Epicenter.Lat - jsonBody.Observation.Lat) / 0.0091) * ((jsonBody.Epicenter.Lat - jsonBody.Observation.Lat) / 0.0091))))
+	output.Scale = float32(randvalue + float64(0.947802 * input.Epicenter.Mag) - 0.004825 * math.Sqrt(float64(((input.Epicenter.Pos.Long - input.Observation.Pos.Long) / 0.0111) * ((input.Epicenter.Pos.Long - input.Observation.Pos.Long) / 0.0111) + ((input.Epicenter.Pos.Lat - input.Observation.Pos.Lat) / 0.0091) * ((input.Epicenter.Pos.Lat - input.Observation.Pos.Lat) / 0.0091))))
 	for i, _ := range output.Scaleranges {
-		output.Scaleranges[i] = int((float32(randvalue) + 0.947802 * jsonBody.Epicenter.Magnitude - (float32(i) + 0.5)) / 0.004825)
+		output.Scaleranges[i] = int((float32(randvalue) + 0.947802 * input.Epicenter.Mag - (float32(i) + 0.5)) / 0.004825)
 		if output.Scaleranges[i] < 0 {
 			output.Scaleranges[i] = 0
 		}
 	}
-	fmt.Println(jsonBody)
+
+	//そのまま座標を返す
+	output.Observation.Lat = input.Observation.Pos.Lat
+	output.Observation.Long = input.Observation.Pos.Long
+	output.Epicenter.Lat = input.Epicenter.Pos.Lat
+	output.Epicenter.Long = input.Epicenter.Pos.Long
+
+	fmt.Println(input)
 	if output.Scale < 0 {
 		output.Status = 2
 		output.Scale = 0
 		output.Result = "Scale is Negative Number!"
 	}
 	rw.WriteHeader(http.StatusOK)
-}
-
-func main() {
-	router := mux.NewRouter()
-
-	// 静的ファイルの提供
-	// $PROROOT/template/map.html が http://localhost:8080/template/map.html でアクセスできる
-	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("."))))
-
-	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
